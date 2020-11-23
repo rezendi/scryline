a7a77a7e234b55fe86dbb8a5220894d05abd0a4a
Jon Evans <jon@rezendi.com>
2020-11-22 17:18:44 -0800

    initial about

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,6 +2,22 @@
 	<title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<h1>About Scryline</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p>
+	Between the pandemic, the US election, Brexit, etc., 2020 has proved it is increasingly hard to make sense of our world.
+	Doing so often requires synthesizing key developments and vital information scattered across articles on different
+	media sites, Medium blogs, Substack posts, tweets, YouTube videos, and more.
+</p>
+
+<p>
+	What's more, traditional journalism often still treats articles as dead once published, rejecting subsequent expert
+	commentary; refrains from linking to primary sources; and fails to provide important context.  As the media landscape
+	grows more fragmented, aggregation tools will become more necessary.
+</p>
+
+<p>
+	Scryline is a simple timeline aggregation tool intended to help provide context and clarity to online news.
+</p>
+
+

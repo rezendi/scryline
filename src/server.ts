bf186ca16186b50e67963d6befec422f39853eb0
Jon Evans <jon@rezendi.com>
2020-11-25 11:15:26 -0800

    session handler

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import session from 'express-session';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
@@ -13,10 +14,18 @@ polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
 		json(),
+		session({
+			secret: process.env.SESSION_SECRET,
+			resave: true,
+			saveUninitialized: true,
+			cookie: {
+				maxAge: 259200
+			},
+		}),
 		sirv('static', { dev }),
 		sapper.middleware({
 			session: (req, res) => ({
-				user: false
+				user: req.session.user
 			})
 		}),
 	)

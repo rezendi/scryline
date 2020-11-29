82c43698b55388e60c4b9871535b5f45d4918eb5
Jon Evans <jon@rezendi.com>
2020-11-28 22:52:36 -0800

    tweak about

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,19 +4,19 @@
 
 <h2>The Scryline Manifesto</h2>
 
-<p>>Our world has fragmented into a kaleidoscope. As 2020's pandemic and election show, what we once thought of as "the news" has splintered into a bewildering panoply of different sources. Real understanding of a complex topic often requires assembly from an cornucopia of traditional media articles, online upstarts, scientific studies, legal documents, experts / pundits on Twitter, eyewitness YouTube clips, Medium posts, Substack essays, Reddit threads, and more.</p>
+<p>Our world has fragmented into a kaleidoscope. As 2020's pandemic and election show, what we once thought of as "the news" has splintered into a bewildering panoply of different sources. Real understanding of a complex topic often requires assembly from an cornucopia of traditional media articles, online upstarts, scientific studies, legal documents, experts / pundits on Twitter, eyewitness YouTube clips, Medium posts, Substack essays, Reddit threads, and more.</p>
 
 <p>"But surely the job of journalism is to make sense of all this!" you cry. Yes... theoretically... but journalism as we know it suffers from a number of flaws which make it unsuitable for this sort of synthesis:</p>
 
 <ul>
     <li>Journalists are generally loath to link to their competitors.</li>
-    <li>They're often even surprisingly reluctant to link to primary sources.</li>
-    <li>They still have a "dead tree" attitude towards articles -- that they are individual and self-contained, and that once published they are set in stone but for corrections -- rather than treating them as connected nodes in a network of living documents.</li>
+    <li>They're often even reluctant to link to primary sources.</li>
+    <li>They still have a "dead tree" attitude towards articles—that they are individual and self-contained, and that once published they are set in stone but for corrections—rather than treating them as connected nodes in a network of living documents.</li>
     <li>The open-source concept of incorporating suggestions from strangers into their articles after publication is unthinkable anathema.</li>
-    <li>Aggregation of others' articles is considered inferior and low-class work.</li>
+    <li>Aggregation of others' articles is considered inferior, low-class work.</li>
 </ul>
 
-<p>Scryline is built on the precept that aggregation is not just potentially illuminating synthesis which can be important and valuable in its own right; rather, in today's fragmented media world, it is increasingly essential in order to provide key context and foster real understanding.</p>
+<p>Scryline is built on the precept that aggregation is not just potentially illuminating synthesis which can be important and valuable in its own right; rather, in today's fragmented media world, it is increasingly <i>essential</i>, in order to provide key context and foster real understanding.</p>
 
 <p>There are of course many ways to aggregate multiple articles. The most obvious and intuitive one is an <em>annotated timeline</em>, in which articles are arranged in simple chronological order, and annotated with brief commentary. Scryline is a home for annotated timelines.</p>
 

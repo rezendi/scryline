923791e01b5386c60a700ac0a53cac91173b2566
Jon Evans <jon@rezendi.com>
2020-11-28 23:06:18 -0800

    manifesto cleanup

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,20 +4,45 @@
 
 <h2>The Scryline Manifesto</h2>
 
-<p>Our world has fragmented into a kaleidoscope. As 2020 has shown us, what we once thought of as "the news" has splintered across a bewildering panoply of different sources. Real understanding of a complex topic often requires assembly from an cornucopia of traditional media articles, online upstarts, scientific studies, legal documents, experts / pundits on Twitter, eyewitness YouTube clips, Medium posts, Substack essays, Reddit threads, and more.</p>
+<p>
+    Our world has fragmented into a kaleidoscope. As 2020 has shown us, what we once thought of as "the news"
+    has splintered across a bewildering panoply of different sources. Real understanding of a complex topic
+    often requires assembly from an array of traditional media articles, online upstarts, scientific studies,
+    legal documents, eyewitness YouTube clips, experts / pundits on Twitter, Medium posts, Substack essays,
+    Reddit threads, and more.
+</p>
 
-<p>"But surely the job of journalism is to make sense of all this for us!" you cry. Yes... theoretically... but journalism as we know it suffers from a number of flaws which make it unsuitable for this sort of synthesis:</p>
+<p>
+    "But surely the job of journalism is to make sense of all this for us!" you cry. Yes... theoretically... but
+    journalism as we know it suffers from a number of flaws which make it unsuitable for this sort of synthesis:
+</p>
 
 <ul>
     <li>Journalists are generally loath to link to their competitors.</li>
     <li>They're often even reluctant to link to primary sources.</li>
-    <li>They still have a "dead tree" attitude towards articles—that they are individual, and self-contained, and once published, set in stone but for corrections—rather than treating them as connected nodes in a network of living documents.</li>
+    <li>They still have a "dead tree" attitude towards articles—that they are individual, and self-contained,
+        and once published, set in stone but for corrections—rather than treating them as connected nodes
+        in a network of living documents.</li>
     <li>The open-source concept of incorporating suggestions from strangers after publication is unthinkable anathema.</li>
     <li>Aggregation of others' articles is considered inferior, low-class work.</li>
 </ul>
 
-<p>Scryline is built on the precept that aggregation is not just potentially illuminating synthesis that can be important in its own right; rather, in today's fragmented media world, it is increasingly <i>essential</i>, to provide key context and foster real understanding.</p>
+<p>
+    Scryline is built on the precept that aggregation is not just potentially illuminating synthesis that can
+    be important in its own right; rather, in today's fragmented media world, <i>aggregation is essential</i>
+    to provide key context and foster real understanding.
+</p>
 
-<p>There are many ways to aggregate multiple articles. But the most obvious and intuitive one is an <em>annotated timeline</em>, in which articles are arranged in simple chronological order, and annotated with brief commentary. Scryline is, simply, a home for annotated timelines.</p>
+<p>
+    There are many ways to aggregate multiple articles. But the most obvious and intuitive is an <em>annotated
+        timeline</em>, in which articles are arranged in simple chronological order, and annotated with brief
+        commentary. Scryline is, simply, a home for annotated timelines.
+</p>
 
-<p>Scryline is also built atop <em>open data</em>. Its timelines are stored as YAML, a human-readable format, in GitHub repositories. A single common public repository is used by default, but it is also possible to use one's own public or private repos. As such, Scryline data will always be accessible to its users/creators, even if Scryline itself were to perish; is automatically available, with complete version histories, via GitHub's rich API; and is easily transferable to other services such as GitLab.</p>
+<p>
+    Scryline is also built atop <em>open data</em>. Its timelines are stored as YAML, a human-readable format,
+    in GitHub repositories. A single common public repository is used by default, but it is also possible to use
+    one's own public or private repos. As such, Scryline data will always be accessible to its users/creators,
+    even if Scryline itself were to perish; is automatically available, with complete version histories, via
+    GitHub's rich API; and is easily transferable to other services such as GitLab.
+</p>

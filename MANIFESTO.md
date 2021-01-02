5a9079f94c455557997af4016e3ad0fd61cb1b97
Jon Evans <jon@rezendi.com>
2021-01-01 19:52:28 -0800

    manifesto and about

--- MANIFESTO.md
+++ MANIFESTO.md
@@ -9,26 +9,33 @@ scientific studies, legal filings, experts / pundits on Twitter, eyewitness
 YouTube clips, Medium posts, Substack essays, Reddit threads, and more.
 
 "But surely the job of journalism is to make sense of all this!" you cry.
-Yes... theoretically... but journalism as we know it suffers from a number
+Yes, theoretically... but journalism as we know it suffers from a number
 of flaws which make it unsuitable for this sort of synthesis:
-* Journalists are generally loath to link even to primary sources, much less
+* Journalists are often loath to link even to primary sources, much less
 their competitors.
-* They still have a "dead tree" attitude that articles are individual,
-self-contained, and once published, set in stone but for corrections, rather
-than treating them as connected nodes in a network of living (but versioned)
-documents.
+* Journalism (and journalistic systems) still have a "dead tree" attitude
+that articles are individual, self-contained, and once published, set in stone
+but for corrections, rather than treating them as connected nodes in a network
+of living (versioned) documents.
 * The open-source concept of incorporating suggestions from strangers into
 their articles after publication is unthinkable anathema.
 * Aggregation and synthesis of others' articles is wrongly considered
 inferior, low-class work.
+* Readers frequently discover individual articles, often on social media,
+without knowing important background or context. Often there is no good single
+source to link to for an overview of that context. This scattershot
+dissemination contributes to the spread of mis- and disinformation.
 
 Scryline is built on the precept that aggregation is not merely useful; in
-today's fragmented media world, it is _essential_, to provide real context and
-understanding.
+today's fragmented media world, to provide real context and understanding, it
+is essential. This makes tools which enable collaborative aggregation highly
+desirable. People already try to use Twitter threads, Wikipedia lists. Medium
+tags, etc., for this purpose; but all are ill-suited to the task.
 
 The most obvious and intuitive form of aggregation is an _annotated timeline_,
-in which articles are arranged in (mostly) chronological order, and annotated
-with brief commentary. Scryline is a home for such timelines.
+in which articles, papers, tweets, preprints, blog posts, etc., are arranged
+in (mostly) chronological order, and annotated with brief commentary. Scryline
+is a home for such timelines.
 
 Scryline is also built atop _hosted data_. Its timelines are stored as YAML, a
 human-readable data-serialization format, in GitHub repos. A single common
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -9,48 +9,55 @@ Email <a href="mailto:info@scryline.com">info@scryline.com</a>.
 <h2>The Scryline Manifesto</h2>
 
 <p>
-    Our world has fragmented into a kaleidoscope. As 2020 has shown us, what we once thought of as "the news"
-    has splintered across a bewildering panoply of different sources. Real understanding of a complex topic
-    often requires assembly from an array of traditional media articles, online upstarts, scientific studies,
-    legal documents, eyewitness YouTube clips, experts / pundits on Twitter, Medium posts, Substack essays,
-    Reddit threads, and more.
+Our world has splintered into different realities. As 2020's pandemic and
+election show, what we once thought of as "the news" has splintered into a
+bewildering kaleidoscope of traditional media articles, online upstarts,
+scientific studies, legal filings, experts / pundits on Twitter, eyewitness
+YouTube clips, Medium posts, Substack essays, Reddit threads, and more.
 </p>
 
 <p>
-    "But surely the job of journalism is to make sense of all this for us!" you cry. Yes... theoretically... but
+    "But surely the job of journalism is to make sense of all this for us!" you cry. Yes, theoretically... but
     journalism as we know it suffers from a number of flaws which make it unsuitable for this sort of synthesis:
 </p>
 
 <ul>
-    <li>Journalists are generally loath to link to their competitors.</li>
-    <li>They're often even reluctant to link to primary sources.</li>
-    <li>They still have a "dead tree" attitude towards articles—that they are individual, self-contained,
-        and once published, set in stone but for corrections—rather than treating them as connected nodes
-        in a network of living documents.</li>
+    <li>Journalists are often loath to link even to primary sources, much less their competitors.</li>
+    <li>Journalism (and journalistic systems) still have a "dead tree" attitude
+        that articles are individual, self-contained, and once published, set in stone
+        but for corrections, rather than treating them as connected nodes in a network
+        of living (versioned) documents.</li>
     <li>The open-source concept of incorporating suggestions from strangers after publication is unthinkable anathema.</li>
-    <li>Aggregation of others' articles is considered inferior, low-class work.</li>
-    <li>Readers frequently only come across individual articles, discovered on social media, without knowing
-        important background or context of the subject under discussion. Often there is no good single source
-        to link to for an overview of that context. This scattershot dissemination contributes to the
-        spread of mis- and disinformation.</li>
+    <li>Aggregation and synthesis of others' articles is wrongly considered inferior, low-class work.</li>
+    <li>Readers frequently discover individual articles, often on social media,
+        without knowing important background or context. Often there is no good single
+        source to link to for an overview of that context. This scattershot
+        dissemination contributes to the spread of mis- and disinformation.</li>
 </ul>
 
 <p>
-    Scryline is built on the precept that aggregation is not just potentially illuminating synthesis that can
-    be important in its own right; rather, in today's fragmented media world, <i>aggregation is essential</i>
-    to provide key context and foster real understanding.
+Scryline is built on the precept that aggregation is not merely useful; in
+today's fragmented media world, to provide real context and understanding, it
+is essential. This makes tools which enable collaborative aggregation highly
+desirable. People already try to use Twitter threads, Wikipedia lists. Medium
+tags, etc., for this purpose; but all are ill-suited to the task.
 </p>
 
 <p>
-    There are many ways to aggregate multiple articles. But the most obvious and intuitive is an <em>annotated
-        timeline</em>, in which articles are arranged in simple chronological order, and annotated with brief
-        commentary. Scryline is, simply, a home for annotated timelines.
+The most obvious and intuitive form of aggregation is an <i>annotated timeline</i>,
+in which articles, papers, tweets, preprints, blog posts, etc., are arranged
+in (mostly) chronological order, and annotated with brief commentary. Scryline
+is a home for such timelines.
 </p>
 
 <p>
-    Scryline is also built atop <em>open data</em>. Its timelines are stored as YAML, a human-readable format,
-    in GitHub repositories. A single common public repository is used by default, but it is also possible to use
-    one's own public or private repos. As such, Scryline data
+Scryline is also built atop <i>hosted data</i>. Its timelines are stored as YAML, a
+human-readable data-serialization format, in GitHub repos. A single common
+public repository is used by default, but it is also possible to use personal
+and/or private repos. As such, Scryline's data is always accessible to its
+users/creators, even if Scryline itself were to perish; is automatically
+available, with complete version histories, via GitHub's rich UI and API; and
+is easily transferable to other services such as GitLab.
 </p>
 <ul>
     <li>will always be accessible to its users/creators, even if Scryline itself were to perish</li>

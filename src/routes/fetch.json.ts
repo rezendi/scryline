c624fe3d1d7cc581d167c336fa6de0c9e840e0f6
Jon Evans <jon@rezendi.com>
2022-09-14 12:06:53 -0400

    kafka scaffolding

---
+++ src/routes/fetch.json.ts
@@ -0,0 +1,21 @@
+const { Kafka } = require('kafkajs');
+const kafka = new Kafka({
+    clientId: 'scryline',
+    brokers: ['localhost:9092']
+});
+
+export async function post(req, res, next) {
+	res.writeHead(200, {
+		'Content-Type': 'application/json'
+	});
+    let data = req.body;
+    const producer = kafka.producer();
+    await producer.connect();
+    await producer.send({
+        topic: 'create-scrylines',
+        messages: [
+            { value: `${data.subject}||${data.duration}||${data.interval}` },
+        ],
+    });    
+    res.end(JSON.stringify({success:true}));
+}
--- src/routes/fetch.svelte
+++ src/routes/fetch.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
     import { stores } from '@sapper/app';
 	const { session } = stores();
+    let subject = '', duration = '', interval = ''
     let durations = ["1 month", "3 months", "6 months", "1 year", "2 years", "5 years", "10 years"];
     let intervals = ["1 day", "1 week", "1 month"];
-	async function fetchLine() {
-        alert("Fetch goes here");
-	}
+
+    async function fetchLine() {
+        // server call goes here
+    }
 </script>
 
 <svelte:head>
@@ -14,12 +16,12 @@
 
 <h2>Fetch New Scryline</h2>
 
-<input id="subject" placeholder="Subject to fetch news about" name="subject" size="80">
+<input id="subject" placeholder="Subject to fetch news about" name="subject" size="80" bind:value={subject}>
 <hr/>
 <b>Duration</b>
 {#each durations as duration}
 	<label>
-		<input type=radio bind:group={durations} name="duration" value={duration}>
+		<input type=radio bind:group={durations} name="duration" bind:value={duration}>
 		{duration}
 	</label>
 {/each}
@@ -27,7 +29,7 @@
 <b>Interval</b>
 {#each intervals as interval}
 	<label>
-		<input type=radio bind:group={intervals} name="interval" value={interval}>
+		<input type=radio bind:group={intervals} name="interval" bind:value={interval}>
 		{interval}
 	</label>
 {/each}

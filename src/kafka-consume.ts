af2f9aad567891bae59ca0e61084441daa6747f3
Jon Evans <jon@rezendi.com>
2022-11-07 14:06:25 -0500

    refactor for testability

--- src/kafka-consume.ts
+++ src/kafka-consume.ts
@@ -16,32 +16,7 @@ const kafka = new Kafka({
 const topic = 'create-scrylines';
 const consumer = kafka.consumer({ groupId: 'consumer-group' });
 
-const fetchTimeline = async (valueString) => {
-  // title, search, duration, interval, user (email, uid, username), existing (slug), iteration
-  let values = JSON.parse(valueString);
-  console.log("values", values);
-
-  let slug = values.existing ? values.existing.slug : util.slugize(values.title);
-  let line = await DB.getLineByUIDAndSlug(values.user.uid, slug);
-
-  // temporarily, just confirm that we can produce from within a consumer
-  if (values.iteration > 0) {
-    console.log("Consumed message produced within previous consumption, woo-hoo");
-    console.log("Found existing line", line);
-  }
-
-  if (!line) {
-    line = {
-      email: values.user.email,
-      title: values.title,
-      userid: values.user.uid,
-      byline: values.user.username,
-      path: util.getPathFor(values.user, ""),
-      editable:false,
-      entries: []
-    };
-  }
-
+const constructQuery = async (values) => {
   // get from and to for API query. from: now - duration + (iteration) intervals, to: from + interval
   let addFun = values.duration.indexOf("year") > 0 ? addYears : addMonths;
   let since = parseInt(values.duration.split(" ")[0]);
@@ -58,12 +33,37 @@ const fetchTimeline = async (valueString) => {
   let api_query = `apiKey=${process.env.NEWS_API_KEY}&q=${values.search}&from=${from}&to=${to}`;
   console.log("api_query", api_query);
 
+  // this is a bit crude, as error handling goes, but makes things more granularly testable
   if (isAfter(now, from_date)) {
     console.log("Cannot get news from the future", from);
-    return;
+    return '';
+  }
+  return api_query
+}
+
+const getLine = async(values) => {
+  let slug = values.existing ? values.existing.slug : util.slugize(values.title);
+  let line = await DB.getLineByUIDAndSlug(values.user.uid, slug);
+  if (values.iteration > 0) {
+    console.log("Found existing line", line);
   }
 
-  let news_api_url = 'https://newsapi.org/v2/everything?' + api_query;
+  if (!line) {
+    line = {
+      email: values.user.email,
+      title: values.title,
+      userid: values.user.uid,
+      byline: values.user.username,
+      path: util.getPathFor(values.user, ""),
+      editable:false,
+      entries: []
+    };
+  }
+  return line;
+}
+
+const runQuery = async(values, query, line) => {
+  let news_api_url = 'https://newsapi.org/v2/everything?' + query;
   let response = await fetch(news_api_url);
   let json = await response.json();
   json.articles.map((a, idx) => {
@@ -82,8 +82,9 @@ const fetchTimeline = async (valueString) => {
   let repo = await Repo.saveLine(line, line.path);
   let dbVals = await DB.saveLine(line.title, values.user.uid, repo.json.content.sha, repo.metadata);
 
+  // OK, fire off the next iteration
   values.iteration = values.iteration + 1;
-  values.existing = { slug: slug };
+  values.existing = { slug: values.existing ? values.existing.slug : util.slugize(values.title) };
   console.log("outgoing values", values);
 
   const producer = kafka.producer();
@@ -96,6 +97,17 @@ const fetchTimeline = async (valueString) => {
         ],
     });    
   }, 10000);
+}
+
+const fetchTimeline = async (valueString) => {
+  // title, search, duration, interval, user (email, uid, username), existing (slug), iteration
+  let values = JSON.parse(valueString);
+  console.log("values", values);
+  let query = await constructQuery(values)
+  if (query.length > 0) { // if we have a valid API query
+    let line = await getLine(values);
+    runQuery(values, query, line);
+  }
 }
 
 const consume = async () => {
